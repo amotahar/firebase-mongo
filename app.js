6f4e8a980f26f4cb23e6d424b2428cef9245304b
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-01-30 15:54:33 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>App</title>
+    <meta name="viewport" content="width=device-width,
+                                   initial-scale=1.0,
+                                   maximum-scale=1.0,
+                                   user-scalable=no,
+                                   minimal-ui">
+    <link rel="stylesheet" href="//cdn.kik.com/app/2.0.1/app.min.css">
+    <style>
+      /* put your styles here */
+    </style>
+  </head>
+  <body>
+    <!-- put your pages here -->
+    <script src="//zeptojs.com/zepto.min.js"></script>
+    <script src="//cdn.kik.com/app/2.0.1/app.min.js"></script>
+    <script>
+      /* put your javascript here */
+    </script>
+  </body>
+</html>
